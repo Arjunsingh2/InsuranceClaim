3adb46e4f00c0ad3418a002e13924c9dad68b6d9
Arjunsingh2 <53118674+Arjunsingh2@users.noreply.github.com>
2021-07-13 01:30:30 +0530

    Add files via upload

---
+++ InsurerData.cs
@@ -0,0 +1,181 @@
+﻿using InsuranceClaim_Microservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsuranceClaim_Microservice
+{
+    public class InsurerData
+    {
+        public static List<InsurerDetail> InsurerDetails = new List<InsurerDetail>()
+        {
+            //new InsurerDetail()
+            //{
+            //    InsurerName="Cognizant",
+            //    InsurerPackageName="Aditya Birla Active Health Platinum",
+            //    InsuranceAmountLimit=15610.0,
+            //    DisbursementDuration="2 Year"
+            //},
+            //new InsurerDetail()
+            //{
+            //    InsurerName="Cognizant",
+            //    InsurerPackageName="Star Health Family Optima Plan",
+            //    InsuranceAmountLimit=17063.0,
+            //    DisbursementDuration="5 years"
+            //},
+            //new InsurerDetail()
+            //{
+            //    InsurerName="Cognizant",
+            //    InsurerPackageName="ICIC Lombard Complete Health Insurence Plane",
+            //    InsuranceAmountLimit=16973.0,
+            //    DisbursementDuration="1 years"
+            //},
+            // new InsurerDetail()
+            //{
+            //    InsurerName="Cognizant",
+            //    InsurerPackageName="Care Health Plan",
+            //    InsuranceAmountLimit=16613.0,
+            //    DisbursementDuration="3 years"
+            //},
+            //  new InsurerDetail()
+            //{
+            //    InsurerName="Cognizant",
+            //    InsurerPackageName="Reliance Health infinity",
+            //    InsuranceAmountLimit=15610.0,
+            //    DisbursementDuration="3 years"
+            //},
+            //   new InsurerDetail()
+            //{
+            //    InsurerName="Cognizant",
+            //    InsurerPackageName="Care Health Plan",
+            //    InsuranceAmountLimit=16613.0,
+            //    DisbursementDuration="3 years"
+            //},
+            //    new InsurerDetail()
+            //{
+            //    InsurerName="Cognizant",
+            //    InsurerPackageName="Care Health Plan",
+            //    InsuranceAmountLimit=16613.0,
+            //    DisbursementDuration="3 years"
+            //},
+            //     new InsurerDetail()
+            //{
+            //    InsurerName="Cognizant",
+            //    InsurerPackageName="Care Health Plan",
+            //    InsuranceAmountLimit=16613.0,
+            //    DisbursementDuration="3 years"
+            //},
+            //      new InsurerDetail()
+            //{
+            //    InsurerName="Cognizant",
+            //    InsurerPackageName="Care Health Plan",
+            //    InsuranceAmountLimit=16613.0,
+            //    DisbursementDuration="3 years"
+            //},
+            //       new InsurerDetail()
+            //{
+            //    InsurerName="Cognizant",
+            //    InsurerPackageName="Care Health Plan",
+            //    InsuranceAmountLimit=16613.0,
+            //    DisbursementDuration="3 years"
+            //}
+
+        };
+        public static List<InitiateClaim> InitiateClaimData = new List<InitiateClaim>()
+        {
+        //    new InitiateClaim()
+        //    {
+        //        Cost = 25000,
+        //        PatientName = "Michal",
+        //        Ailment = "Low Back Pain",
+        //        TreatmentPackageName = "Orthopedic",
+        //        InsurerName = "Cognizant"
+
+        //    },
+        //    new InitiateClaim()
+        //    {
+        //         Cost = 25000,
+        //        PatientName = "Robert",
+        //        Ailment = "Acne Vulgaris",
+        //        TreatmentPackageName = "Skin",
+        //        InsurerName = "Cognizant"
+
+        //    },
+        //    new InitiateClaim()
+        //    {
+        //         Cost = 60000,
+        //        PatientName = "Patrik",
+        //        Ailment = "Indigestion & Heartburn",
+        //        TreatmentPackageName = "Gastro",
+        //        InsurerName = "Cognizant"
+
+        //    },
+        //    new InitiateClaim()
+        //    {
+        //        Cost = 60000,
+        //        PatientName = "Johny",
+        //        Ailment = "Constipation",
+        //        TreatmentPackageName = "Gastro",
+        //        InsurerName = "Cognizant"
+
+        //    },
+        //    new InitiateClaim()
+        //    {
+        //        Cost = 6000,
+        //        PatientName = "Christine",
+        //        Ailment = "Toothache",
+        //        TreatmentPackageName = "Dental",
+        //        InsurerName = "Cognizant"
+
+        //    },
+        //    new InitiateClaim()
+        //    {
+        //        Cost = 5500,
+        //        PatientName = "Oliver",
+        //        Ailment = "Asthma",
+        //        TreatmentPackageName = "Respira",
+        //        InsurerName = "Cognizant"
+
+        //    },
+        //    new InitiateClaim()
+        //    {
+        //        Cost = 10000,
+        //        PatientName = "Harry",
+        //        Ailment = "Femur Fracture",
+        //        TreatmentPackageName = "Orthopedic",
+        //        InsurerName = "Cognizant"
+
+        //    },
+          
+        //    new InitiateClaim()
+        //{
+        //        Cost = 10000,
+
+        //    PatientName = "Emma",
+        //        Ailment = "Sore throat",
+        //        TreatmentPackageName = "Gastro",
+        //        InsurerName = "Cognizant"
+
+        //    },
+        //    new InitiateClaim()
+        //    {
+        //        Cost = 3000,
+        //        PatientName = "Rohn",
+        //        Ailment = "Kidney Stone",
+        //        TreatmentPackageName = "Urology",
+        //        InsurerName = "Cognizant"
+
+        //    },
+        //    new InitiateClaim()
+        //    {
+        //        Cost = 3000,
+        //        PatientName = "Bucky",
+        //        Ailment = "Brain hemorrhage",
+        //        TreatmentPackageName = "Neuro",
+        //        InsurerName = "Cognizant"
+
+        //    },
+           };
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,87 @@
+using InsuranceClaim_Microservice.Models;
+using InsuranceClaim_Microservice.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceClaim_Microservice
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = Configuration["Jwt:Issuer"],
+            ValidAudience = Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+        };
+    });
+            services.AddControllers();
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "InsuranceClaim_Microservice", Version = "v1" });
+            });
+            services.AddDbContext<InsurerDBContext>(context => {
+                context.UseSqlServer(Configuration.GetConnectionString("InsuranceClaim_MicroService"));
+            });
+            services.AddScoped<IInusrerRepo, InsurerRepo>();
+            services.AddScoped<IInitiateClaimRepo, InitiateClaimrepo>();
+            services.AddScoped<DbContext, InsurerDBContext>();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseAuthentication();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InsuranceClaim_Microservice v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
